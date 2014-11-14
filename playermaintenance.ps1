47ff7b0f0abda4b874bc4f319ccd295a117da94b
Tostito <tostitogaming@live.com>
2014-11-13 20:30:55 -0600

    updated for new XML nodes
    updated for new XML nodes

--- playermaintenance.ps1
+++ playermaintenance.ps1
@@ -40,11 +40,11 @@
     #wipe orphaned id's (permanent death issue) if dead player owns nothing.
     [string]$compare = "360"
     $nodePIDs = $myXML2.SelectNodes("//Identities/MyObjectBuilder_Identity"  , $ns2)
-    $nodeOwns = $myXML.SelectNodes("//SectorObjects/MyObjectBuilder_EntityBase[(@xsi:type='MyObjectBuilder_CubeGrid')]/CubeBlocks/MyObjectBuilder_CubeBlock[Owner='$playerid']"  , $ns)
     ForEach($node in $nodePIDs){
-        $NPCID = [string]$node.PlayerId.InnerText[0] + [string]$node.PlayerId.InnerText[1] + [string]$node.PlayerId.InnerText[2]
+        $NPCID = [string]$node.PlayerId[0] + [string]$node.PlayerId[1] + [string]$node.PlayerId[2]
         $playerid = $node.PlayerId
-        $clientcount=$myXML2.SelectNodes("//ConnectedPlayers/dictionary/item[Value='$playerid'] | //DisconnectedPlayers/dictionary/item[Value='$playerid']" , $ns2).count
+        $client = $myXML2.SelectSingleNode("//DisconnectedPlayers/dictionary/item[Value='$playerid']" , $ns2)
+        $clientcount= $client.count
         $nodeOwns = $myXML.SelectNodes("//SectorObjects/MyObjectBuilder_EntityBase[(@xsi:type='MyObjectBuilder_CubeGrid')]/CubeBlocks/MyObjectBuilder_CubeBlock[Owner='$playerid']"  , $ns).count
         IF($clientcount -eq 0 -and $nodeOwns -eq 0 -and $NPCID -ne $compare){
             $selectdelete = $myXML2.SelectSingleNode("//Factions/Factions/MyObjectBuilder_Faction/Members/MyObjectBuilder_FactionMember[PlayerId='$playerid']" , $ns2)
@@ -112,11 +112,9 @@
     Add-Content -Path $playerspath -Value "="
     Add-Content -Path $playerspath -Value "Player Cleanup ========="
     $nodePIDs = $myXML2.SelectNodes("//Identities/MyObjectBuilder_Identity"  , $ns2)
-    $nodeClientID=$myXML2.SelectNodes("//ConnectedPlayers/dictionary/item | //DisconnectedPlayers/dictionary/item" , $ns2) 
     ForEach($node in $nodePIDs){
-        ForEach($node3 in $nodeClientID){
-            IF($node3.Value.InnerText -eq $node.PlayerId){
-                $nodename = $node3.Key.ClientId
+                $nodeClientID=$myXML2.SelectSingleNode("//DisconnectedPlayers/dictionary/item[Value='$($node.PlayerId)']" , $ns2)
+                $nodename = $nodeClientID.ParentNode.Item.Key.ClientId
                 $nodeid = $node.PlayerId
                 Add-Content -Path $playerspath -Value "="
                 Add-Content -Path $playerspath -Value "Checking [$($node.DisplayName)] ..."
@@ -133,12 +131,11 @@
                   Try{$selectdelete.ParentNode.RemoveChild($selectdelete)}
                   Catch{Write-Host -ForegroundColor Green "[$($node.DisplayName)] has no faction join requests, proceeding..."}
                   Add-Content -Path $playerspath -Value "Deleting [$nodename] [$nodeid] [$($node.DisplayName)]"
-                  $node3.ParentNode.RemoveChild($node3)
+                  Try{$nodeClientID.ParentNode.RemoveChild($nodeClientID)}
+                  Catch{Write-Host -ForegroundColor Green "[$($node.DisplayName)] has no connection status, proceeding..."}
                   $node.ParentNode.RemoveChild($node)
                   $deletedplayer = $deletedplayer + 1
                 } 
-            }
-        }
     }
 
     #factioncleaning
